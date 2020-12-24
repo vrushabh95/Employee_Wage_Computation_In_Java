2167b7aafaed64b1a0666f5d2857c82df0fbc32d
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-24 16:32:20 +0530

    Adding Part Time Employee And Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,14 +2,17 @@ class EmpWageComputation {
 
 	public static void main(String args[]) {
 		// CONSTANTS
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		//VARIABLES
 		int empHrs = 0;
 		int empWage = 0;
 		//COMPUTATION
-		int empCheck = (int) ( Math.random() * 10 ) % 2;
-		if ( empCheck == IS_FULL_TIME )
+		int empCheck = (int) ( Math.random() * 10 ) % 3;
+		if ( empCheck == IS_PART_TIME )
+			empHrs = 4;
+		else if ( empCheck == IS_FULL_TIME )
 			empHrs = 8;
 		else
 			empHrs = 0;
