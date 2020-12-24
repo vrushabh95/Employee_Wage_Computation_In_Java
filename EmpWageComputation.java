0206c4ce6ada45fcb8dca79a3e3db7f3f3b9f7af
vrushabh95 <mundhe.vrushabh@gmail.com>
2020-12-24 16:46:41 +0530

    Calculating Employee Wage Till A Condition Of Total Working Hours Is Reached For A Month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,12 +4,15 @@ public class EmpWageComputation {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String args[]) {
 		//VARIABLES
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		//COMPUTATION
-		for ( int day = 1; day <= NUM_OF_WORKING_DAYS; day++ ) {
+		while ( totalEmpHrs <= MAX_HRS_IN_MONTH &&
+					totalWorkingDays < NUM_OF_WORKING_DAYS )	{
+			totalWorkingDays++;
 			int empCheck = (int) ( Math.random() * 10 ) % 3;
 			switch (empCheck)	{
 				case IS_PART_TIME:
@@ -21,10 +24,10 @@ public class EmpWageComputation {
 				default:
 					empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage for Day " + day +": "+ empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +  empHrs);
 		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
